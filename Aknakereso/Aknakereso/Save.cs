c3ca037da8ce5c3afe4e0361c5ece1c83154c47c
fmarkyy98 <fmark98@gmail.com>
2017-11-29 09:05:06 +0100

    Prototype1

---
+++ Aknakereso/Aknakereso/Save.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Aknakereso
+{
+    static class Saves
+    {
+        public static void Save(Aknamezo Gamestate,string path)
+        {
+            StreamWriter f = new StreamWriter(path,false,Encoding.UTF8);
+            f.WriteLine(Gamestate.getLengt(0) + " " + Gamestate.getLengt(1));
+            f.Close();
+        }
+        public static Aknamezo Load(string path)
+        {
+            StreamReader f = new StreamReader(path);
+            string[] s = f.ReadLine().Split(';');
+            
+            Aknamezo BeviteliAknamezo = new Aknamezo();
+            //aknamező construct f.Readline().Split(' ');
+            f.Close();
+            return BeviteliAknamezo;
+        }
+    }
+}
