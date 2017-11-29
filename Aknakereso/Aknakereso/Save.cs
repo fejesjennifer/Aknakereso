4d3e66fe10eccef55fc4bcacf1733067940d8cd9
fmarkyy98 <fmark98@gmail.com>
2017-11-29 09:50:49 +0100

    Prototype3

--- Aknakereso/Aknakereso/Save.cs
+++ Aknakereso/Aknakereso/Save.cs
@@ -12,17 +12,18 @@ namespace Aknakereso
         public static void Save(Aknamezo Gamestate, string path)
         {
             StreamWriter f = new StreamWriter(path, false, Encoding.UTF8);
-            int m = Gamestate.getLength(0);
-            int n = Gamestate.getLength(1);
+            int m = Gamestate.GetLength(0);
+            int n = Gamestate.GetLength(1);
             f.WriteLine(m + ";" + n);
             for (int i = 0; i < m; i++)
             {
                 string s = "";
                 for (int j = 0; j < n - 1; j++)
                 {
+                    s += Gamestate[i, j].value + "," + Gamestate[i, j].visible + "," + Gamestate[i, j].flagged + ";";
                 }
-
-                f.WriteLine();
+                s += Gamestate[i, n - 1].value + "," + Gamestate[i, n - 1].visible + "," + Gamestate[i, n - 1].flagged;
+                f.WriteLine(s);
             }
             f.Close();
         }
