da1f78d4199ca87faf6258b9c92271cae9d3f84a
fejesjennifer <fejesjenni@gmail.com>
2017-11-29 09:03:49 +0100

    mezo struct generated

---
+++ Aknakereso/Aknakereso/Aknamezo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aknakereso
+{
+    class Aknamezo
+    {
+       public struct mezo
+        {
+            public int value;
+            public bool visible;
+            public bool flagged;
+            
+        }
+
+        //mezo matrix = new mezo[];
+
+        public Aknamezo ()
+        {
+            mezo[,] matrix = new mezo[n,m];
+
+        }
+
+    }
+}
