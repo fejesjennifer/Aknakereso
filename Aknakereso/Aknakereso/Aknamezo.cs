e7e44a24e5a10a47967b0404aae0863cdce9c433
fejesjennifer <fejesjenni@gmail.com>
2017-11-29 09:41:53 +0100

    Szenvedés. KÉSZ!!

--- Aknakereso/Aknakereso/Aknamezo.cs
+++ Aknakereso/Aknakereso/Aknamezo.cs
@@ -8,20 +8,49 @@ namespace Aknakereso
 {
     class Aknamezo
     {
-       public struct mezo
+        public struct mezo
         {
             public int value;
             public bool visible;
             public bool flagged;
-            
+
         }
 
-        //mezo matrix = new mezo[];
-
-        public Aknamezo ()
+        mezo[,] matrix;
+        public Aknamezo(Aknamezo a)
         {
-            mezo[,] matrix = new mezo[n,m];
+            Aknamezo b = new Aknamezo(a.GetLength(0), a.GetLength(1));
 
+            for (int i = 0; i < b.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    b[i, j] = a[i, j];
+                }
+            }
+        }
+        public Aknamezo(int n, int m)
+        {
+            matrix = new mezo[n, m];
+
+
+        }
+        public mezo[,] getAknamezo(int n, int m)
+        {
+            mezo[,] a = new mezo[n, m];
+            return a;
+        }
+
+        public int GetLength(int dimension)
+        {
+            return matrix.GetLength(dimension);
+        }
+
+        public mezo this[int i, int j]
+        {
+            get { return matrix[i, j]; }
+            set { matrix[i, j] = value; }
         }
 
     }
